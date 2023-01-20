3c559c53503818bcdbc3e5ad3c56f2f0ca56f6b4
Rohit <rk212300@outlook.com>
2023-01-20 13:41:27 +0530

    Minor changes

--- src/DSA/Matrix/FindPair.java
+++ src/DSA/Matrix/FindPair.java
@@ -1,53 +1,63 @@
 package DSA.Matrix;
 
+import java.util.Arrays;
+
 // Given an n x n matrix mat[n][n] of integers, find the maximum value of mat(c, d) – mat(a, b) over all choices of
 // indexes such that both c > a and d > b.
 public class FindPair {
     public static void main(String[] args) {
         int[][] arr = {
-                {-111, 2, -1, -4, -20},
-                {-8, -3, 4, 2, 1},
-                {3, 8, 6, 1, 3},
-                {-4, -1, 1, 7, -6},
-                {0, 40, 10, -5, 10}
+                { 1, 2, -1, -4, -20 },
+                { -8, -3, 4, 2, 1 },
+                { 3, 8, 6, 1, 3 },
+                { -4, -1, 1, 7, 6 },
+                { 0, -4, 10, -5, 1
+                }
         };
-        
+
         System.out.println(findMaxValue(arr, 5));
     }
-    
+
     static int findMaxValue(int[][] mtx, int n) {
         // ans variable
         int ans = Integer.MIN_VALUE;
-        
+
         // maxArr[i][j] stores max of elements in matrix
         // from (i, j) to (N-1, N-1)
         int[][] maxArr = new int[n][n];
-        
+
         maxArr[n - 1][n - 1] = mtx[n - 1][n - 1];
 
-//        Preprocessing the last column.
+        // Preprocessing the last column.
         int maxVal = mtx[n - 1][n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            if (mtx[i][n - 1] > maxVal) maxVal = mtx[i][n - 1];
-            
+            if (mtx[i][n - 1] > maxVal)
+                maxVal = mtx[i][n - 1];
+
             maxArr[i][n - 1] = maxVal;
         }
-//        Preprocessing the last row.
+        // Preprocessing the last row.
         maxVal = mtx[n - 1][n - 1];
         for (int j = n - 2; j >= 0; j--) {
-            if (mtx[n - 1][j] > maxVal) maxVal = mtx[n - 1][j];
-            
+            if (mtx[n - 1][j] > maxVal)
+                maxVal = mtx[n - 1][j];
+
             maxArr[n - 1][j] = maxVal;
         }
-//        Processing the rest of matrix from bottom.
+
+        // Processing the rest of matrix from bottom.
         for (int i = n - 2; i >= 0; i--) {
             for (int j = n - 2; j >= 0; j--) {
                 if ((mtx[i + 1][j + 1] - mtx[i][j]) > ans) {
                     ans = maxArr[i + 1][j + 1] - mtx[i][j];
                 }
-                
-                maxArr[i][j] = Math.max(maxArr[i][j], Math.max(maxArr[i + 1][j], maxArr[i][j]));
+
+                maxArr[i][j] = Math.max(mtx[i][j], Math.max(maxArr[i + 1][j], maxArr[i][j + 1]));
             }
+        }
+
+        for (int[] a : maxArr) {
+            System.out.println(Arrays.toString(a));
         }
         return ans;
     }
--- src/DSA/Matrix/MedianInRowSorted.java
+++ src/DSA/Matrix/MedianInRowSorted.java
@@ -1,36 +1,34 @@
-package DSA.Matrix;
 // Find median in a row wise sorted matrix
 
 import java.util.Arrays;
 
 //Given a row wise sorted matrix of size RxC where R and C are always odd, find the median of the matrix.
 public class MedianInRowSorted {
-    
-    int median(int[][] m, int r, int c) {
+
+    static int median(int[][] m, int r, int c) {
         int high = Integer.MIN_VALUE;
         int low = Integer.MAX_VALUE;
-        
-        
+
         for (int i = 0; i < r; i++) {
             // Finding the minimum element
             low = Math.min(low, m[i][0]);
-            
+
             // Finding the maximum element
             high = Math.max(high, m[i][c - 1]);
         }
-        
+
         int desired = (r * c + 1) / 2;
         while (low < high) {
             int mid = low + (high - low) / 2;
-            
+
             int place = 0;
             int get = 0;
-            
+
             // Find count of elements smaller than mid
             for (int i = 0; i < r; ++i) {
-                
+
                 get = Arrays.binarySearch(m[i], mid);
-                
+
                 // If element is not found in the array the
                 // binarySearch() method returns
                 // (-(insertion_point) - 1). So once we know
@@ -40,27 +38,34 @@ public class MedianInRowSorted {
                 if (get < 0) {
                     get = Math.abs(get) - 1;
                 }
-                
+
                 // If element is found in the array it returns
                 // the index(any index in case of duplicate). So we go to last
-                // index of element which will give  the number of
+                // index of element which will give the number of
                 // elements smaller than the number including
                 // the searched element.
                 else {
                     while (get < m[i].length && m[i][get] == mid)
                         get++;
                 }
-                
+
                 place = place + get;
             }
-            
+
             if (place < desired) {
                 low = mid + 1;
-            }
-            else {
+            } else {
                 high = mid;
             }
         }
         return low;
+    }
+
+    // main function
+
+    public static void main(String[] args) {
+        int[][] arr = { { 1, 3, 5 }, { 2, 6, 9 }, { 3, 6, 9 } };
+        System.out.println(median(arr, arr.length, arr[0].length));
+
     }
 }
